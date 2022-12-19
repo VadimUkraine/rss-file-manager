a5eb3f11252d4dbf45633d939dc3c444a9388f5f
v.oleynik <v.oleynik@itransition.com>
2022-12-19 22:10:55 +0200

    fix: change cat file manager command

--- src/fs/cat.js
+++ src/fs/cat.js
@@ -1,25 +1,25 @@
-import { createReadStream } from 'fs';
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
-import { pipeline } from 'stream/promises';
-import getError, { INVALID_INPUT_ERROR } from '../helpers/errorsHandler.js';
+import { createReadStream } from "fs";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
+import { finished } from "stream/promises";
+import getError, { INVALID_INPUT_ERROR } from "../helpers/errorsHandler.js";
 
-const catAction = async (parameters) => {  
-    try{
-      if (parameters.length === 0) {
-        throw new Error(INVALID_INPUT_ERROR);
-      }
-
-      await readFile(resolve(parameters[0]), { encoding: 'utf8' });
-
-      const writableToTerminal = process.stdin;
-      const filePath = (resolve(parameters[0]));
-      const readFromFile = createReadStream(filePath, { encoding: 'utf8' }); 
-
-      await pipeline(readFromFile, writableToTerminal);
-    } catch (error) {
-      getError(error);
+const catAction = async (parameters) => {
+  try {
+    if (parameters.length === 0) {
+      throw new Error(INVALID_INPUT_ERROR);
     }
+
+    await readFile(resolve(parameters[0]), { encoding: "utf8" });
+
+    const filePath = resolve(parameters[0]);
+    const readFromFile = createReadStream(filePath, { encoding: "utf8" });
+
+    readFromFile.pipe(process.stdout);
+    await finished(readFromFile);
+  } catch (error) {
+    getError(error);
+  }
 };
 
 export default catAction;
